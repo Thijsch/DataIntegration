1ef2dbb15bbccfac054a7a676875b20f93015cd4
JannekeNouwen <j.nouwen@student.han.nl>
2022-06-07 11:33:56 +0200

    add metadata

--- snaketwo.py
+++ snaketwo.py
@@ -1,19 +1,22 @@
 # 23-5-2022
 # Met dit snakefile kan snpEFF gerund worden over de vcf files. Deze data
 # wordt vervolgens ingelezen en in de database gezet
-from pathlib import Path
-import os
 import glob
+import json
+import os
+from datetime import datetime
+from pathlib import Path
+
 import dotenv
 
+from inserter import Inserter
 from PDF_reader import PdfReader
 from Vcf_reader import VcfReader
-from inserter import Inserter
 
 dotenv.load_dotenv(".env")
 
-
 def snakeextra():
+    get_metadata()
     raw = []
     vcf_dir = str(os.getenv("VCF_FILES"))
     if vcf_dir.endswith("/"):
@@ -51,3 +54,17 @@ def snakeextra():
     inserter.insert_data()
     inserter.validate()
     inserter.close_connection()
+
+
+def get_metadata():
+    """Get metadata for workflow"""
+    metadata = {
+        "time": str(datetime.now()),
+        "vcf files": str(os.getenv("VCF_DIR")),
+        "pdf_files": str(os.getenv("PDF_DIR")),
+        "snpeff file": str(os.getenv("SNPEFF_FILE")),
+        "run by user": str(os.getenv("USER"))
+    }
+
+    with open('metadata.json', 'w') as fp:
+        json.dump(metadata, fp, indent=4)
